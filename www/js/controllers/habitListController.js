fb63f45a3e652594d7148d6e902f661c576ede10
seescode <seesyong@gmail.com>
2015-01-25 20:42:22 -0500

    Created the initial habit app

---
+++ www/js/controllers/habitListController.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+(function () {
+    angular.module('habit')
+      .controller('habitListController', function ($scope) {
+          $scope.data = {
+              states: [
+                  { text: 'Cue', cssClass: 'cue' },
+                  { text: 'Routine', cssClass: 'routine' },
+                  { text: 'Reward', cssClass: 'reward' }
+              ]
+          };
+
+          $scope.data.active = $scope.data;
+          $scope.data.index = 0;
+      });
+})();
---
+++ www/js/directives/stateSlider.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+
+(function () {
+    angular.module('habit')
+      .directive('stateSlider', function () {
+          return {
+              restrict: 'EA',
+              template: '<div class="state-slider" ng-click="next()"><div class="{{t.cssClass}}" ng-class="state($index)" ng-repeat="t in stateData" ng-bind="getText($index)"></div></div>',
+              scope: {
+                  stateData: "=",
+                  selectedIndex: "="
+              },
+              replace: true,
+              link: function (scope, element, attrs, ctrl) {
+                  scope.next = function () {
+                      scope.selectedIndex = scope.selectedIndex + 1;
+
+                      if (scope.stateData.length == scope.selectedIndex) {
+                          scope.selectedIndex = 0;
+                      }
+                  }
+
+                  scope.state = function (id) {
+                      if (scope.selectedIndex == id) {
+                          return 'state';
+                      }
+
+                      return 'state-collapsed';
+                  }
+
+                  scope.getText = function (id) {
+                      if (scope.selectedIndex == id) {
+                          return scope.stateData[id].text;
+                      }
+
+                      return '';
+                  }
+              }
+          }
+      });
+})();
+
+
+
