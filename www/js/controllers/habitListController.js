eff776c858a65179dd581d527fe519425479f2e0
seescode <seesyong@gmail.com>
2015-01-27 21:17:13 -0500

    Hardcoded some habits

--- www/js/controllers/habitListController.js
+++ www/js/controllers/habitListController.js
@@ -3,15 +3,24 @@
 (function () {
     angular.module('habit')
       .controller('habitListController', function ($scope) {
-          $scope.data = {
-              states: [
-                  { text: 'Cue', cssClass: 'cue' },
-                  { text: 'Routine', cssClass: 'routine' },
-                  { text: 'Reward', cssClass: 'reward' }
-              ]
-          };
+          $scope.data = [
+              {
+                  states: [
+                     { text: 'Finish shower', cssClass: 'cue' },
+                     { text: 'Make one commit', cssClass: 'routine' },
+                     { text: 'Play game', cssClass: 'reward' }
+                  ],
+                  index: 0
+              },
+              {
+                  states: [
+                     { text: 'Finish breakfast', cssClass: 'cue' },
+                     { text: 'Eat Vitamin', cssClass: 'routine' },
+                     { text: 'No reward', cssClass: 'reward' }
+                  ],
+                  index: 1
+              }
+          ];
 
-          $scope.data.active = $scope.data;
-          $scope.data.index = 0;
       });
 })();
