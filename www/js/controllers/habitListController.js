821d44b2e645e4754efad2a0b8008d707a1aa536
seescode <seesyong@gmail.com>
2015-02-16 22:08:33 -0500

    Wrote a sync data service for getting items.  It's not working though

--- www/js/controllers/habitListController.js
+++ www/js/controllers/habitListController.js
@@ -2,7 +2,7 @@
 
 (function () {
     angular.module('habit')
-      .controller('habitListController', function ($scope) {
+      .controller('habitListController', function ($scope, syncDataService) {
           //$scope.data = [
           //    {
           //        states: [
@@ -22,21 +22,32 @@
           //    }
           //];
 
+          syncDataService.getHabits().
+              success(function (data, status, headers, config) {
+                  // this callback will be called asynchronously
+                  // when the response is available
+                  $scope.data = data;
+              }).
+              error(function (data, status, headers, config) {
+                  // called asynchronously if an error occurs
+                  // or server returns response with an error status.
+              });
 
-          $scope.data = [
-              {
-                  "StateText": [
-                      "Finish shower", "Make one commit", "Play game"
-                  ],
-                  "Index": 0
-              },
-              {
-                  "StateText": [
-                      "Finish breakfast", "Eat Vitamin", "No reward"
-                  ],
-                  "Index": 1
-              }
-          ];
+
+          //$scope.data = [
+          //    {
+          //        "StateText": [
+          //            "Finish shower", "Make one commit", "Play game"
+          //        ],
+          //        "Index": 0
+          //    },
+          //    {
+          //        "StateText": [
+          //            "Finish breakfast", "Eat Vitamin", "No reward"
+          //        ],
+          //        "Index": 1
+          //    }
+          //];
 
       });
 })();
---
+++ www/js/services/syncDataService.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+(function () {
+    angular.module('habit').factory('syncDataService', function ($http) {
+
+        var getHabits = function () {
+            //TODO use $resource
+            //TODO do not hardcode the server url
+            var promise = $http.get('http://localhost:59919/api/sync');
+
+            return promise;
+        },
+
+        postHabitStats = function (stats) {
+            //TODO need to post habit stats to the server.
+
+        };
+
+
+        return {
+            getHabits: getHabits,
+            postHabitStats: postHabitStats
+        };
+    });
+})();
