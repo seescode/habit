d16097ae34911d79c06ee26609ddf804ebad5f7e
seescode <seesyong@gmail.com>
2015-02-28 20:21:04 -0500

    Making the sync button actually sync down habits

--- www/js/controllers/habitListController.js
+++ www/js/controllers/habitListController.js
@@ -3,44 +3,15 @@
 (function () {
     angular.module('habit')
       .controller('habitListController', function ($scope, syncWebService) {
-          //$scope.data = [
-          //    {
-          //        states: [
-          //           { text: 'Finish shower', cssClass: 'cue' },
-          //           { text: 'Make one commit', cssClass: 'routine' },
-          //           { text: 'Play game', cssClass: 'reward' }
-          //        ],
-          //        index: 0
-          //    },
-          //    {
-          //        states: [
-          //           { text: 'Finish breakfast', cssClass: 'cue' },
-          //           { text: 'Eat Vitamin', cssClass: 'routine' },
-          //           { text: 'No reward', cssClass: 'reward' }
-          //        ],
-          //        index: 1
-          //    }
-          //];
+          $scope.data = null;
 
-          syncWebService.query(function (data) {
-              $scope.data = data;
-          });
+          $scope.sync = function () {
+              syncWebService.query(function (data) {
+                  $scope.data = data;
+              });
+          };
 
 
-          //$scope.data = [
-          //    {
-          //        "StateText": [
-          //            "Finish shower", "Make one commit", "Play game"
-          //        ],
-          //        "Index": 0
-          //    },
-          //    {
-          //        "StateText": [
-          //            "Finish breakfast", "Eat Vitamin", "No reward"
-          //        ],
-          //        "Index": 1
-          //    }
-          //];
 
       });
 })();
