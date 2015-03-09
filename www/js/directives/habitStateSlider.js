ab27c6980555caf1860b96ba7ba1b33d04df4cf5
seescode <seesyong@gmail.com>
2015-03-08 22:40:46 -0400

    Saving todos

--- www/js/directives/habitStateSlider.js
+++ www/js/directives/habitStateSlider.js
@@ -27,10 +27,20 @@
                   scope.stateData[2].cssClass = 'reward';
 
                   scope.prevStateClick = function (selectedIndex) {
+                      //TODO this method seems unnecessary.  
                       alert(selectedIndex);
                   };
 
                   scope.nextStateClick = function (selectedIndex) {
+                      //TODO: basically you need to something like this:
+
+                      //when selectedIndex is 2
+                      //data.habit.completionDates.add(moment().utc().startOf('year');)
+                      //habitDataService.save(data);
+
+                      //else just remove the completiondate and save it.
+                      
+
                       alert(selectedIndex);
                   };
               }
