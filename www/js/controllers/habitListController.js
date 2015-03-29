1e8407720a7e11418c925783fe1fcfa195666934
seescode <seesyong@gmail.com>
2015-03-28 21:21:00 -0400

    Got angular fire setup

--- www/js/controllers/habitListController.js
+++ www/js/controllers/habitListController.js
@@ -2,8 +2,18 @@
 
 (function () {
     angular.module('habit')
-      .controller('habitListController', function (syncWebService, habitDataService, habitService) {
+      .controller('habitListController', function ($scope, syncWebService, habitDataService, habitService, $firebaseObject) {
           var vm = this;
+
+          //https://vivid-fire-159.firebaseio.com/#-Jl1bNvbPj1v7Ke56pLi|67a1f7cd09fd2c233251c5aa2b5538eb
+          var ref = new Firebase("https://vivid-fire-159.firebaseio.com");
+          // download the data into a local object
+
+          // download the data into a local object
+          var syncObject = $firebaseObject(ref);
+          // synchronize the object with a three-way data binding
+          // click on `index.html` above to see it used in the DOM!
+          syncObject.$bindTo($scope, "fire");
 
           habitDataService.read().then(function (data) {
               if (data != null) {
