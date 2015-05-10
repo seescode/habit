6ffe51938d9f040155642fe9e6c4d08b85f9e7d5
seescode <seesyong@gmail.com>
2015-05-09 23:04:00 -0400

    minor updates

--- www/js/directives/habitStateEntry.js
+++ www/js/directives/habitStateEntry.js
@@ -12,13 +12,6 @@
               replace: false,
               link: function (scope, element, attrs, ctrl) {
                   scope.submit = function () {
-                      //Need to figure out the structure of habits first.
-                      //then we need to do something like habits.cow check null then create []
-                      //then do a push to add the new items in there.
-                      console.log(scope.cue);
-                      console.log(scope.routine);
-                      console.log(scope.reward);                     
-                      
                       
                       if (scope.habits == null) {
                           scope.habits = [];
